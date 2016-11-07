2b1fd63ec8c99f5f4ffb5e2b9db8df6b54df71f7
Brandon Kowalski <btk44@cornell.edu>
2016-11-07 10:29:21 -0500

    Initial commit.

---
+++ workday-cache/workday-distcache/src/main/java/org/workday/distcache/App.java
@@ -0,0 +1,13 @@
+package org.workday.distcache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
